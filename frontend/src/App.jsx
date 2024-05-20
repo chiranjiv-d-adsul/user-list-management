8a5c470b62f03408cf0851e2694a98b307fff58f
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-05-20 20:58:24 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import CreateList from "./components/CreateList";
 import UserList from "./components/UserList";
 
-// axios.defaults.baseURL = "https://user-list-backend-1.onrender.com/chiru/v1/";
-axios.defaults.baseURL = "http://localhost:5000/chiru/v1/";
+axios.defaults.baseURL = "https://user-list-backend-1.onrender.com/chiru/v1/";
+// axios.defaults.baseURL = "http://localhost:5000/chiru/v1/";
 
 axios.defaults.withCredentials = true;
 
